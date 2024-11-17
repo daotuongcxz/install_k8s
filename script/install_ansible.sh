0d6447c7846aac788c1474dca52d7076b8e2e08a
truongnguyen250902@gmail.com <truongnguyen250902@gmail.com>
2024-11-18 01:39:17 +0700

    update

--- script/install_ansible.sh
+++ script/install_ansible.sh
@@ -8,28 +8,39 @@ print_success() { echo -e "\e[32m[SUCCESS] $1\e[0m"; }
 print_error() { echo -e "\e[31m[ERROR] $1\e[0m"; exit 1; }
 
 # Variables
+DEB_DIR="../resources/debs/"
 DOCKER_VERSION="26.0.0"
 HELM_VERSION="v3.13.0"
 SSH_KEY_PATH="./keyssh"  
 node_01_ip="10.10.1.252"
 node_02_ip="10.10.1.115"
 node_03_ip="10.10.1.213"
-node_01_user="ubuntu"
-node_02_user="ubuntu"
-node_03_user="ubuntu"
+node_01_user="root"
+node_02_user="root"
+node_03_user="root"
+
+ANSIBLE_CORE_DEB="${DEB_DIR}ansible-core_2.16.8-1ppa~mantic_all.deb"
+ANSIBLE_DEB="${DEB_DIR}ansible_5.10.0.deb"
+
 
 # Install necessary packages
 install_packages() {
     print_info "Installing required packages: tree, zip, ansible, sshpass, docker, docker-compose, helm..."
     apt update -y && apt upgrade -y
-    apt install -yq tree zip ansible sshpass curl apt-transport-https gnupg lsb-release
-
-    Install Docker
-    if ! command -v docker &>/dev/null; then
-        curl -fsSL -o install_docker.sh https://raw.githubusercontent.com/docker/docker-install/master/install.sh
-        chmod +x install_docker.sh
-        sh ./install_docker.sh --version "$DOCKER_VERSION" || print_error "Failed to install Docker"
+    apt install -yq tree zip sshpass curl apt-transport-https gnupg lsb-release
+    
+    #install ansible >= 2.15.5	
+    if [[ ! -f "$ANSIBLE_CORE_DEB" || ! -f "$ANSIBLE_DEB" ]]; then
+    echo "Error: One or more required .deb files are missing in $DEB_DIR"
+    exit 1
     fi
+
+    echo "Installing ansible-core..."
+    sudo dpkg -i "$ANSIBLE_CORE_DEB"
+
+    echo "Installing ansible..."
+    sudo dpkg -i "$ANSIBLE_DEB"
+
 
     # Install Helm
     if ! command -v helm &>/dev/null; then
